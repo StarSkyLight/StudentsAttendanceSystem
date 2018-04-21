ea26ac6ec239edf2915cc0b47d9cf3be9f81214f
StarSkyLight <stark_09@foxmail.com>
2018-04-21 20:08:53 +0800

    添加了教师查询点名详情的方法（未完全测试）

---
+++ src/main/java/com/sas/controller/AttendanceController.java
@@ -0,0 +1,57 @@
+package com.sas.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+import com.sas.entity.AttendanceEntity;
+import com.sas.entity.StudentEntity;
+import com.sas.mapper.AttendanceMapper;
+import com.sas.mapper.StudentMapper;
+
+@RestController
+@RequestMapping("/attendancecontrol")
+public class AttendanceController {
+	
+	@Autowired
+	private AttendanceMapper attendanceMapper;
+	@Autowired
+	private StudentMapper studentMapper;
+	
+	@RequestMapping(value="/getallattendance")
+	public String getAllAttendance(@RequestParam String checkInfor) throws Exception{
+		String returnStr = "";
+		
+		if(checkInfor != null && !checkInfor.equals("")){
+			List<AttendanceEntity> list = new ArrayList<AttendanceEntity>();
+			list = attendanceMapper.getAttendanceByCheckId(checkInfor);
+			
+			List<Map<String,String>> templist = new ArrayList<Map<String,String>>();
+			Gson gson = new Gson();
+			for(AttendanceEntity tempAttendance : list){
+				String tempStuId = tempAttendance.getStudentId();
+				StudentEntity tempStudent = studentMapper.getStudent(tempStuId);
+				
+				Map<String,String> tempMap = new HashMap<String,String>();
+				
+				
+				tempMap.put("attendanceEntity", gson.toJson(tempAttendance));
+				tempMap.put("studentEntity", gson.toJson(tempStudent));
+				
+				templist.add(tempMap);
+			}
+			
+			returnStr = gson.toJson(templist);
+		}
+		
+		return returnStr;
+	}
+
+}
